f407ddc48b81349c5cef5cf396d940af6a95756b
consupalabra <consupalabrahoy@gmail.com>
2025-08-26 18:01:23 -0300

    v1.1 webapp.py

--- webapp.py
+++ webapp.py
@@ -19,44 +19,26 @@ def copy_to_clipboard_button(label, text_to_copy):
 st.title("Procesador de Texto Interlineal")
 st.write("Ingresa dos textos y el programa los unirá línea por línea.")
 
-# Inicializar st.session_state si no existe
-if "texto1" not in st.session_state:
-    st.session_state.texto1 = ""
-if "texto2" not in st.session_state:
-    st.session_state.texto2 = ""
-if "resultado" not in st.session_state:
-    st.session_state.resultado = ""
-
 col1, col2 = st.columns(2)
 
 with col1:
-    texto1 = st.text_area("Texto 1", height=300, key="texto1", value=st.session_state.texto1)
+    texto1 = st.text_area("Texto 1", height=300)
 
 with col2:
-    texto2 = st.text_area("Texto 2", height=300, key="texto2", value=st.session_state.texto2)
-
-# El botón de limpiar restablece el estado de la aplicación.
-# Se usa st.session_state para manejar el estado de las áreas de texto.
-if st.button("Limpiar campos"):
-    st.session_state.texto1 = ""
-    st.session_state.texto2 = ""
-    st.session_state.resultado = ""
-    st.experimental_rerun()
+    texto2 = st.text_area("Texto 2", height=300)
 
 if st.button("Procesar"):
-    lista1 = st.session_state.texto1.split("\n")
-    lista2 = st.session_state.texto2.split("\n")
+    lista1 = texto1.split("\n")
+    lista2 = texto2.split("\n")
 
     resultado_str = ""
     # Se utiliza min() para asegurar que el bucle no exceda el texto más corto
     for i in range(min(len(lista1), len(lista2))):
         resultado_str += f"{lista1[i]}\n{lista2[i]}\n"
+
+    # Mostrar el resultado
+    st.text_area("Resultado", resultado_str, height=400, key="resultado_final")
     
-    st.session_state.resultado = resultado_str
-
-# Mostrar el resultado
-st.text_area("Resultado", st.session_state.resultado, height=400, key="resultado_final")
-
-# Botón para copiar el resultado al portapapeles
-# La función se llama con el texto que se quiere copiar
-copy_to_clipboard_button("Copiar resultado", st.session_state.resultado)
+    # Botón para copiar el resultado al portapapeles
+    # La función se llama con el texto que se quiere copiar
+    copy_to_clipboard_button("Copiar resultado", resultado_str)
