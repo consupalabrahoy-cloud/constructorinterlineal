cf70386d6a9b423a913faed0d4fed896a9f595a7
consupalabra <consupalabrahoy@gmail.com>
2025-08-26 16:14:46 -0300

    Add files via upload
    Aplicación de constructor de interlineal. Subida de archivos.

---
+++ webapp.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from st_copy_to_clipboard import st_copy_to_clipboard
+
+st.title("Procesador de Texto Interlineal")
+st.write("Ingresa dos textos y el programa los unirá línea por línea.")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    texto1 = st.text_area("Texto 1", height=300)
+
+with col2:
+    texto2 = st.text_area("Texto 2", height=300)
+
+if st.button("Procesar"):
+    lista1 = texto1.split("\n")
+    lista2 = texto2.split("\n")
+
+    resultado_str = ""
+    for i in range(min(len(lista1), len(lista2))):
+        resultado_str += f"{lista1[i]}\n{lista2[i]}\n"
+
+    st.text_area("Resultado", resultado_str, height=400, key="resultado_final")
+    
+    # Nuevo botón para copiar
+    st_copy_to_clipboard(resultado_str)
